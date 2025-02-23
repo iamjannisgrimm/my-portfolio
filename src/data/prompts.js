c64a4dc62f0e0403776ca334fb165bbab7891b2b
Jannis <jannis@grimm.me>
2025-02-22 19:07:55 -0500

    Updated prompts

--- src/data/prompts.js
+++ src/data/prompts.js
@@ -1,8 +1,8 @@
 // /src/data/prompts.js
 const conversationPrompts = [
-    "Tell me about yourself",
-    "What is your greatest skill?",
-    "How did you get started in your career?",
+    "What’s a project you’re proud of that most people wouldn’t notice at first glance?",
+    "How do you approach balancing creativity with practicality in your work?",
+    "When learning something new, what’s your process for moving from curiosity to mastery?",
     "What do you enjoy doing in your free time?",
     "What's your favorite project?"
   ];
